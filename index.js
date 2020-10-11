e817a6d3979d2794dd0e5f555c76dcda9a2b5b88
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-10 23:32:02 -0700

    the routes from index.js file is moved into routes folders/api folder

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const members = require('./members');
 const logger = require('./middleware/logger')
 const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(logger)
+app.use(logger);
 
-//get all members:
-app.get('/api/members', (req, res) => {
-  res.json(members);
-});
+//members api routes:
+app.use('/api/members', require('./routes/api/members'))
 
-//get single members:
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)))
-  } else{
-    res.status(400).json({ msg: 'member not found'});
-  }
-})
 
 
 app.listen(PORT, () => {
---
+++ routes/api/members.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const members = require('../../members');
+
+//get all members:
+router.get('/', (req, res) => {
+  res.json(members);
+});
+
+//get single members:
+router.get('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  } else{
+    res.status(400).json({ msg: 'member not found'});
+  }
+})
+module.exports =router;
