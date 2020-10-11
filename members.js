b65e28b0bbe89b005418bb3d136ae63ffa03ce5c
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-10 22:58:51 -0700

    the dummy data of members are moved into the members.js file

---
+++ members.js
@@ -0,0 +1,21 @@
+const members=[
+  {
+    id:1,
+    name:"Jerry",
+    phone:'123345678',
+    email:'sha@970909.com'
+  },
+  {
+    id:2,
+    name:"Amy",
+    phone:'7233455478',
+    email:'Amy@970909.com'
+  },
+  {
+    id:3,
+    name:"Cici",
+    phone:'778902328',
+    email:'cici@980909.com'
+  }
+];
+module.exports = members;
