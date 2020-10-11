cd392cf26b1c9229e03a42c387e7449bc9dc575b
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-10 22:28:46 -0700

    script in package.json file is changed with start and dev

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app =express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res)=>{
-  res.send('<h1>heyheyhey</h1>')
+  res.send('<h1>heyheyhey wcccc</h1>')
 })
 
 app.listen(PORT,()=>{
