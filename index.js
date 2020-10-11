3d203a76d71c74854b809b5ba9fa563b337c5880
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-10 23:20:27 -0700

    api route is created for getting individual member

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const path = require('path'); 
-const app =express();
-const members = require('./members'); 
+const path = require('path');
+const app = express();
+const members = require('./members');
 const logger = require('./middleware/logger')
 const PORT = process.env.PORT || 5000;
- 
-app.use(logger) 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger)
 
-
-app.get('/api/members', (req,res)=>{
+//get all members:
+app.get('/api/members', (req, res) => {
   res.json(members);
 });
 
-//set up a static folder 
-app.use(express.static(path.join(__dirname,'public')))
+//get single members:
+app.get('/api/members/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  } else{
+    res.status(400).json({ msg: 'member not found'});
+  }
+})
 
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`)
-});  
+});
 
