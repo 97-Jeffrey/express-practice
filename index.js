97cff340337f6192b9929b1a9b552cc313669ddd
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-10-10 22:59:56 -0700

    another middleware function is created to sbow url of request when each request is sent

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path'); 
-
 const app =express();
-
+const members = require('./members'); 
 const PORT = process.env.PORT || 5000;
 
+const logger=(req,res, next)=>{
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  next()
+}
+
+app.use(logger) 
+
+
+app.get('/api/members', (req,res)=>{
+  res.json(members);
+});
 
 //set up a static folder 
 app.use(express.static(path.join(__dirname,'public')))
